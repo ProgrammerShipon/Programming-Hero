94e432ca9e11cf1368566e67c3014cba8ef33c8b
Shipon Hossen Raju <msshipon234@gmail.com>
2023-01-29 09:29:57 +0600

    some problem and Qustions

---
+++ Milestone/Milestone-4,-Hello-JavaScript/Module-19.5-Practice-variable-Array-Conditionals-Loop/19_5-1 Recap and Practice Variable, Array, Conditionals/main.js
@@ -0,0 +1,95 @@
+
+
+/*
+      VARIABLE
+ 
+ 1. What is Javascript?
+ 2. How does JS Works?
+ 3. What is Variable?
+ 4. Declare a variable?
+ 5. types of variable? How can you find out type of a variable?
+ 6. Primitive and non-primitive data types?
+ 7. Naming Convention of JS variables 
+ 8. Math Operation +, -, *, /, %
+ 9. Short hand: +=, -=, *= /=
+ 10. ++, --
+ 11. ParseInt, ParseFloat
+ 12. toFixed
+
+*/
+
+
+
+/*
+
+      ARRAY
+
+ 1. What is the purpose?
+ 2. How to declare an array in JS?
+ 3. Number of elements?
+ 4. what is index ?
+ 5. Find the value of an element by?
+ 6. Change an element by the value>
+ 7. get the index of an element by the value?
+ 8. What does it mean when you get undefined while getting the value of an element by index?
+ 9. How can you add an element to an array at the last position?
+ 10. How can you remove the last element from array?
+ 11. Add an element at the first position of an array?
+ 12. Remove the first element of an array?
+
+*/
+
+
+
+
+/*
+
+      CONDITIONS
+
+ 1. meaning of: >, <, >=, <=, ==, !=, ===, !==
+ 2. meaning of &&
+ 3. meaning of ||
+ 4. Let's say you have x amount of money. if you have more than 80,000 , then you will by a mac
+    if more than 60,000 then you will by gaming laptop
+    if you more than 40,000 then you will by lenovo Yoga
+    if you have more than 20,000 then you will by an used  laptop
+    Otherwise, you will use you mobile phone
+*/
+
+
+
+
+
+/*
+
+      LOOP
+
+ 1. Display 'Ajke amar mon valo nei' for 39 time
+ 2. Display numbers between 58 to 98
+ 3. Show all even numbers from 412 to 456
+ 4. Show all odd number 581 to 623
+ 5. Difference bet ween while loop and  for loop
+ 6. Declare an array for all the topics that you have learned last few days display then as output
+ 7. Create an array for all the mobile phones. Display all the elements of the array by using a while loop
+ 8. Run a loop from 30 to 86. this loop will stop if the values get higher than 44
+ 9. Write the price of the books that you have . Display the prices if the prices is lower than 200
+
+
+
+*/
+
+
+
+/**
+ * 
+ * 
+ *       Solve this All problem 
+ *          Go to : Practice/Js-Problem-Solving
+ * 
+ * 
+ */
+
+
+
+
+
