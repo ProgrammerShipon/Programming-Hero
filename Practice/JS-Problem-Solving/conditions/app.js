a187d223671a79a91d54a96aabdb02f62838bb91
Shipon Hossen Raju <msshipon234@gmail.com>
2023-01-29 12:57:28 +0600

    Condition - Your amount choice

---
+++ Practice/JS-Problem-Solving/conditions/app.js
@@ -0,0 +1,16 @@
+console.clear();
+
+var amount = 19000;
+
+if (amount >= 80000) {
+  console.log("You will by a mac");
+} else if (amount >= 60000) {
+  console.log("You will by gaming laptop");
+} else if (amount >= 40000) {
+   console.log('You will by lenovo Yoga')
+} else if (amount >= 20000) {
+   console.log('You will by an used laptop')
+} else if (amount < 20000) {
+   console.log('You will use Mobile')
+}
+
