deaf8609335b65e91a74d315d323ac23c445e07b
Shipon Hossen Raju <msshipon234@gmail.com>
2023-01-30 22:24:55 +0600

    problem solved and 4 way

--- Milestone/Milestone-4,-Hello-JavaScript/Module-20;Core-concepts-functions-and-objects/problem-solving/app.js
+++ Milestone/Milestone-4,-Hello-JavaScript/Module-20;Core-concepts-functions-and-objects/problem-solving/app.js
@@ -5,17 +5,15 @@
  *
  */
 
-function foo() {
-  console.log("foo");
-  return bar();
-}
-// foo();
-
 function bar() {
   console.log("bar");
-  return foo();
 }
-// bar();
+
+function foo() {
+  bar();
+  console.log("foo");
+}
+foo();
 
 /**
  * --==== Problem solving 2 =====--
@@ -43,8 +41,20 @@ function make_avg(arrays) {
     total += arrays[i];
   }
   const avg = total / arrLength;
+  return avg;
 }
-make_avg(arrs);
+var myAvg = make_avg(arrs);
+console.log(myAvg);
+
+function make_avgs(arrays, size) {
+  var total = 0;
+  for (let i = 0; i < size; i++) {
+    total += arrays[i];
+  }
+  const avg = total / size;
+  return avg;
+}
+console.log(make_avgs([23, 34, 45], 3));
 
 /**
  * --==== Problem solving 4 =====--
@@ -55,15 +65,12 @@ make_avg(arrs);
  */
 function odd_even(num) {
   if (num % 2 == 0) {
-    console.log( num , ' Even Number');
+    console.log(num, " Even Number");
   } else {
-    console.log(num , ' Is Odd Number')
+    console.log(num, " Is Odd Number");
   }
 }
-odd_even(50)
-
-
-
+odd_even(50);
 
 /**
  * --==== Problem solving 5 =====--
@@ -71,6 +78,7 @@ odd_even(50)
  *   you are in a hurry to go to your school on time . But when you are crossing the road , the traffic signal is red coloured. in this situation is, if you try to cross the road you may be in danger . if you notice a yellow coloured traffic signal, you should stop. if you notice a green coloured traffic signal, you should cross the road. So write a JS code, where there is a variable called signal. its value can be green, yellow or red & we will get different activities as output depending on the variable . so hurry up.
  *  */
 
+// --======== 1 way
 var signal = "yellow";
 
 if (signal == "red") {
@@ -83,20 +91,46 @@ if (signal == "red") {
   console.log(" is Not a road !");
 }
 
+// --======== 2 way
+
 switch (signal) {
   case "red":
     console.log("the road you may be in danger");
     break;
-  
+
   case "yellow":
     console.log("you should stop");
     break;
-  
+
   case "green":
     console.log("you should cross the road");
     break;
-  
+
   default:
     console.log(" is Not a road signal!");
     break;
 }
+
+// --======== 3 way
+
+var colors = ["red", "green", "blue"];
+
+function singnals(colors) {
+  console.log(colors);
+  for (let color in colors) {
+    var col = colors[color];
+
+    if (col == "green") {
+      console.log("you should cross the road");
+    }
+  }
+}
+singnals(colors)
+
+// --======== 4 way
+
+colors.forEach((color) => {
+  if (color == "red") {
+    console.log("the road you may be in danger");
+  }
+});
