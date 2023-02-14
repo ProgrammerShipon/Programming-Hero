607e984ef47628bd8f22e9729dc89867002bad3f
Shipon Hossen Raju <msshipon234@gmail.com>
2023-02-14 20:20:23 +0600

    complite discount fixed

--- Practice/JS-Projects/Discount-calculator/simple-fixe-discount/js/main.js
+++ Practice/JS-Projects/Discount-calculator/simple-fixe-discount/js/main.js
@@ -1,2 +1,26 @@
+function getInputValue(id) {
+  const element = document.getElementById(id);
+  const values = parseFloat(element.value);
+  element.value = "";
+  return values;
+}
 
+function getElementsValue(id) {
+  let element = document.getElementById(id);
+  element = parseFloat(element.innerText);
+  return element;
+}
 
+function setInnerText(id, values) {
+  let element = document.getElementById(id);
+  element.innerText = values;
+}
+
+document.getElementById("btn-discount").addEventListener("click", function () {
+  const inputValue = getInputValue("input-value");
+  const discount = getElementsValue("discount");
+  const total = inputValue - inputValue * (discount / 100);
+
+  setInnerText("YourValue", inputValue);
+  setInnerText("show-value", total);
+});
