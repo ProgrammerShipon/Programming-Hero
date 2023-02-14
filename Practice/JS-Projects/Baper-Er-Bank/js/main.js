02632612daf1c0fad4c9b59c1a9bce41fcdaeeaa
Shipon Hossen Raju <msshipon234@gmail.com>
2023-02-14 11:39:50 +0600

    Baaper Bank - only Functional program

--- Practice/JS-Projects/Baper-Er-Bank/js/main.js
+++ Practice/JS-Projects/Baper-Er-Bank/js/main.js
@@ -6,21 +6,62 @@ function getElementsByID(ID) {
 }
 
 function getInputsByID(ID) {
-  let element = document.getElementById(ID);
-  element = parseFloat(element.value);
-  return element;
+  const element = document.getElementById(ID);
+  const values = parseFloat(element.value);
+  return values;
+}
+
+function addition(num1, num2) {
+  return num1 + num2;
+}
+
+function minus(num1, num2) {
+  return num1 - num2;
+}
+
+function innerTexts(id, value) {
+  let element = document.getElementById(id);
+  element.innerText = value;
+}
+
+function emty(id) {
+  document.getElementById(id).value = "";
 }
 
 // Click Events
 document.getElementById("deposit-btn").addEventListener("click", depositFc);
 
 function depositFc() {
-  getElementsByID("total-deposit");
+  const deposit = addition(
+    getElementsByID("total-deposit"),
+    getInputsByID("deposit-field")
+  );
+  const balance = addition(
+    getInputsByID("deposit-field"),
+    getElementsByID("total-balance")
+  );
+  if (!isNaN(deposit) && !isNaN(balance)) {
+    innerTexts("total-deposit", deposit);
+    innerTexts("total-balance", balance);
+    emty("deposit-field");
+  }
 }
 
 // ----===============================-----
 
 document.getElementById("withdraw-btn").addEventListener("click", withdrawFc);
 function withdrawFc() {
-  getInputsByID("withdraw-field");
+  const withdraw = addition(
+    getElementsByID("total-withdraw"),
+    getInputsByID("withdraw-field")
+  );
+  const balance = minus(
+    getElementsByID("total-balance"),
+    getInputsByID("withdraw-field")
+  );
+  if (!isNaN(withdraw) && !isNaN(balance)) {
+    innerTexts("total-withdraw", withdraw);
+    innerTexts("total-balance", balance);
+    emty("withdraw-field");
+  }
 }
