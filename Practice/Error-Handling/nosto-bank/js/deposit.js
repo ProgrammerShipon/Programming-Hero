223ab1a346ee3240761afab4b94ef9e645597e32
Shipon Hossen Raju <msshipon234@gmail.com>
2023-03-07 08:20:49 +0600

    bug fixed

---
+++ Practice/Error-Handling/nosto-bank/js/deposit.js
@@ -0,0 +1,29 @@
+// DRY ---> Do Not Repeat Yourself
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  /* 
+    1. get the element by id
+    2. get the value from the element
+    3. convert string value to a number
+    */
+  const newDepositAmount = getInputFieldValueById("deposit-field");
+  /* 
+    1. get previous deposit total by id
+
+    */
+  const previousDepositTotal = getTextElementValueById("deposit-total");
+
+  // calculate new deposit total
+  const newDepositTotal = parseFloat(previousDepositTotal) + newDepositAmount;
+  console.log(
+    newDepositTotal,
+    parseFloat(previousDepositTotal),
+    newDepositAmount
+  );
+  // set deposit total value
+  setTextElementValueById("deposit-total", newDepositTotal);
+
+  // get previous balance by using the function
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+  const newBalanceTotal = parseFloat(previousBalanceTotal) + newDepositTotal;
+  setTextElementValueById("balance-total", newBalanceTotal);
+});
