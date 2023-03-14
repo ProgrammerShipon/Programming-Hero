a5adf746d1ffde3c44db43cc93225a9f8c0f2a23
Shipon Hossen Raju <msshipon234@gmail.com>
2023-03-14 10:49:30 +0600

    today practice complite

---
+++ Practice/Error-Handling/broken-phone/js/app.js
@@ -0,0 +1,122 @@
+let phonesArr = 0;
+const loadPhones = async (searchText, dataLimit) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhones(data.data, dataLimit);
+  phonesArr = data.data;
+  console.log(phonesArr);
+};
+
+const displayPhones = (phones, dataLimit) => {
+  console.log(phones);
+  const phonesContainer = document.getElementById("phones-container");
+  phonesContainer.innerHTML = "";
+  // display 10 phones only
+  const showAll = document.getElementById("show-all");
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, dataLimit);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
+
+  // display no phones found
+  const noPhone = document.getElementById("no-found-message");
+  if (phones.length === 0) {
+    noPhone.classList.remove("d-none");
+  } else {
+    noPhone.classList.add("d-none");
+  }
+
+  console.log(phones[0]);
+
+  // display all phones
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
+        <div class="card p-4">
+            <div>
+              <img src="${phone.image}" class="card-img-top" alt="${phone.phone_name}">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+            </div>
+        </div>
+      `;
+    phonesContainer.appendChild(phoneDiv);
+  });
+  // stop spinner or loader
+  toggleSpinner(false);
+};
+
+const processSearch = (dataLimit) => {
+  toggleSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadPhones(searchText, dataLimit);
+  searchField.value = "";
+};
+
+// handle search button click
+document.getElementById("btn-search").addEventListener("click", function () {
+  // start loader
+  processSearch(10);
+});
+
+// search input field enter key handler
+document
+  .getElementById("search-field")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      processSearch(10);
+    }
+  });
+
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } else {
+    loaderSection.classList.add("d-none");
+  }
+};
+// not the best way to load show All
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  displayPhones(phonesArr);
+});
+
+const loadPhoneDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetails(data.data);
+};
+
+const displayPhoneDetails = (phone) => {
+  const modalTitle = document.getElementById("details-title");
+  modalTitle.innerText = phone.name;
+  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.innerHTML = `
+        <p>Release Date: ${
+          phone.releaseDate ? phone.releaseDate : "No releaseDate"
+        }</p>
+        <p>Storage and Others Info : ${
+          phone.mainFeatures
+            ? Object.entries(phone.mainFeatures).map(
+                (item) => "<br/>" + item.join(" : ")
+              )
+            : "No Information"
+        }</p>
+        <p>Sensor: ${
+          phone.mainFeatures.sensors
+            ? phone.mainFeatures.sensors[0]
+            : "no sensor"
+        }</p>
+    `;
+};
+
+loadPhones("apple");
