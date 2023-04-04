4f7d651ec0c869a2c30cf847f188f6a0aaa1c326
Shipon Hossen Raju <msshipon234@gmail.com>
2023-04-04 09:55:59 +0600

    daisyUI , Router setup and test

---
+++ Projects/Module/54.5-The-Meal/The-PS-MealDB/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+    theme: {
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+	},
+  // add daisyUI plugin
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: 'dark',	
+    themes: false,
+  },
+};
